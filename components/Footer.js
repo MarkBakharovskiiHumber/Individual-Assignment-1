493d195ab093f762c8a7d7edf3ea52ff21a81b46
MarkBakharovskiiHumber <Bma20.02@mail.ru>
2026-03-08 13:33:17 -0400

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      Built with <span>♥</span> using React &amp; Next.js — Front-End Development Assignment
+    </footer>
+  );
+}
